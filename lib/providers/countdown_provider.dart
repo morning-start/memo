714c08f8f08b8cb210beb22fd827f7085e3ca88f
MorningStart <morning-start@foxmail.com>
2025-03-01 19:39:24 +0800

    feat(countdown_provider): 添加倒计时任务的数据库持久化功能
    - 在 CountdownNotifier 中添加了数据库初始化和任务加载方法
    - 实现了任务的添加、删除、更新和状态切换的数据库操作
    - 优化了任务数据的处理逻辑，提高了数据的可靠性和完整性

--- lib/providers/countdown_provider.dart
+++ lib/providers/countdown_provider.dart
@@ -1,23 +1,81 @@
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:memo/tools/db_seeder.dart';
 import '../models/countdown_model.dart';
+import 'package:sqflite/sqflite.dart';
 
 class CountdownNotifier extends StateNotifier<List<Countdown>> {
-  CountdownNotifier() : super([]);
+  late Database _db;
 
-  void addTask(Countdown task) {
+  CountdownNotifier() : super([]) {
+    _initializeDatabase();
+  }
+
+  Future<void> _initializeDatabase() async {
+    _db = await initializeDatabase(Countdown.toSqlCreateTable);
+
+    await _loadTasks();
+  }
+
+  Future<void> _loadTasks() async {
+    final List<Map<String, dynamic>> maps =
+        await _db.query(Countdown.tableName);
+
+    state = List.generate(maps.length, (i) {
+      return Countdown.fromMap(maps[i]);
+    });
+  }
+
+  Future<void> addTask(Countdown task) async {
+    await _db.insert(
+      Countdown.tableName,
+      task.toMap(),
+      conflictAlgorithm: ConflictAlgorithm.replace,
+    );
+
     state = [...state, task];
   }
 
-  void removeTask(String id) {
+  Future<void> removeTask(String id) async {
+    await _db.delete(
+      Countdown.tableName,
+      where: 'id = ?',
+      whereArgs: [id],
+    );
+
     state = state.where((task) => task.id != id).toList();
   }
 
-  void updateTask(String id, Countdown updatedTask) {
+  Future<void> toggleTask(String id) async {
+    final updatedTask = state.firstWhere((task) => task.id == id);
+    updatedTask.changeStatus();
+    if (updatedTask.isCompleted && updatedTask.isRecurring) {
+      updatedTask.restart();
+    }
+    await _db.update(
+      Countdown.tableName,
+      updatedTask.toMap(),
+      where: 'id = ?',
+      whereArgs: [id],
+    );
     state = state.map((task) => task.id == id ? updatedTask : task).toList();
   }
 
+  Future<void> updateTask(String id, String newTitle, DateTime newStartTime,
+      Duration newDuration, bool newIsRecurring) async {
+    final updatedTask = state.firstWhere((task) => task.id == id);
+    updatedTask.update(newTitle, newStartTime, newDuration, newIsRecurring);
+    await _db.update(
+      Countdown.tableName,
+      updatedTask.toMap(),
+      where: 'id = ?',
+      whereArgs: [id],
+    );
+
+    state = state.map((task) => task.id == id ? updatedTask : task).toList();
+  }
 }
 
-final countdownProvider = StateNotifierProvider<CountdownNotifier, List<Countdown>>(
+final countdownProvider =
+    StateNotifierProvider<CountdownNotifier, List<Countdown>>(
   (ref) => CountdownNotifier(),
 );
--- lib/providers/todo_provider.dart
+++ lib/providers/todo_provider.dart
@@ -18,20 +18,15 @@ class TodoListNotifier extends StateNotifier<List<Todo>> {
   Future<void> _loadTodos() async {
     final List<Map<String, dynamic>> maps = await _db.query(Todo.tableName);
     state = List.generate(maps.length, (i) {
-      return Todo(
-        id: maps[i]['id'],
-        title: maps[i]['title'],
-        deadline: DateTime.parse(maps[i]['deadline']),
-        isCompleted: maps[i]['isCompleted'] == 1,
-      );
+      return Todo.fromMap(maps[i]);
     });
   }
 
   /// 添加一个新的任务到待办事项列表中。
   ///
   /// 参数:
-  ///   - [title]: 任务的标题。
-  ///   - [deadline]: 任务的截止日期。
+  ///   - title: 任务的标题。
+  ///   - deadline: 任务的截止日期。
   Future<void> addTodo(String title, DateTime deadline) async {
     final newTodo = Todo(title: title, deadline: deadline);
     await _db.insert(
@@ -45,15 +40,11 @@ class TodoListNotifier extends StateNotifier<List<Todo>> {
   /// 切换指定ID的任务的完成状态。
   ///
   /// 参数:
-  ///   - [id]: 要切换完成状态的任务的ID。
+  ///   - id: 要切换完成状态的任务的ID。
   Future<void> toggleTodo(String id) async {
-    final todo = state.firstWhere((todo) => todo.id == id);
-    final updatedTodo = Todo(
-      id: todo.id,
-      title: todo.title,
-      deadline: todo.deadline,
-      isCompleted: !todo.isCompleted,
-    );
+    final updatedTodo = state.firstWhere((todo) => todo.id == id);
+    updatedTodo.changeStatus();
+    
     await _db.update(
       Todo.tableName,
       updatedTodo.toMap(),
@@ -69,7 +60,7 @@ class TodoListNotifier extends StateNotifier<List<Todo>> {
   /// 从待办事项列表中移除指定ID的任务。
   ///
   /// 参数:
-  ///   - [id]: 要移除的任务的ID。
+  ///   - id: 要移除的任务的ID。
   Future<void> removeTodo(String id) async {
     await _db.delete(
       Todo.tableName,
@@ -82,18 +73,13 @@ class TodoListNotifier extends StateNotifier<List<Todo>> {
   /// 更新指定ID的任务的标题和截止日期。
   ///
   /// 参数:
-  ///   - [id]: 要更新的任务的ID。
-  ///   - [newTitle]: 任务的新标题。
-  ///   - [newDeadline]: 任务的新截止日期。
+  ///   - id: 要更新的任务的ID。
+  ///   - newTitle: 任务的新标题。
+  ///   - newDeadline: 任务的新截止日期。
   Future<void> updateTodo(
       String id, String newTitle, DateTime newDeadline) async {
-    final todo = state.firstWhere((todo) => todo.id == id);
-    final updatedTodo = Todo(
-      id: todo.id,
-      title: newTitle,
-      deadline: newDeadline,
-      isCompleted: todo.isCompleted,
-    );
+    final updatedTodo = state.firstWhere((todo) => todo.id == id);
+    updatedTodo.update(newTitle, newDeadline);
     await _db.update(
       Todo.tableName,
       updatedTodo.toMap(),
