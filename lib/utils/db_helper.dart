ed9ca2614947cbd64f631c7a8f42ae666d1fee1a
MorningStart <morning-start@foxmail.com>
2025-03-02 08:48:34 +0800

    feat(database): 添加数据库操作方法并优化初始化流程
    - 为 DatabaseHelper 类添加了多个数据库操作方法，包括打开表、插入数据、查询数据、更新数据和删除数据
    - 优化了数据库初始化流程，增加了创建表的 SQL 语句列表参数
    - 添加了数据库关闭方法

--- lib/utils/db_helper.dart
+++ lib/utils/db_helper.dart
@@ -23,6 +23,12 @@ class DatabaseHelper {
     return _database!;
   }
 
+  /// 初始化数据库并返回 Database 对象。
+  ///
+  /// 如果数据库文件不存在，则创建新数据库并执行传入的 SQL 语句列表。
+  ///
+  /// 参数：
+  ///   - createSqlList - 创建表的 SQL 语句列表。
   Future<Database> _initDatabase(List<String> createSqlList) async {
     return await openDatabase(
       join(await getDatabasesPath(), _dbName),
@@ -32,6 +38,85 @@ class DatabaseHelper {
         }
       },
       version: _dbVersion,
+    );
+  }
+
+  Future<Database> openTable(String sql) async {
+    return await openDatabase(
+      join(await getDatabasesPath(), _dbName),
+      onCreate: (db, version) async {
+        await db.execute(sql);
+      },
+      version: _dbVersion,
+    );
+  }
+
+  Future<void> close() async {
+    final db = await _instance.db;
+    await db.close();
+  }
+
+  Future<void> insert(String table, Map<String, dynamic> row) async {
+    final db = await _instance.db;
+    await db.insert(
+      table,
+      row,
+      conflictAlgorithm: ConflictAlgorithm.replace,
+    );
+  }
+
+  Future<List<Map<String, dynamic>>> query(
+    String table,
+    bool? distinct,
+    List<String>? columns,
+    String? where,
+    List<Object?>? whereArgs,
+    String? groupBy,
+    String? having,
+    String? orderBy,
+    int? limit,
+    int? offset,
+  ) async {
+    final db = await _instance.db;
+    return await db.query(table,
+        distinct: distinct,
+        columns: columns,
+        where: where,
+        whereArgs: whereArgs,
+        groupBy: groupBy,
+        having: having,
+        orderBy: orderBy,
+        limit: limit,
+        offset: offset);
+  }
+
+  Future<int> update(
+    String table,
+    Map<String, Object?> values, {
+    String? where,
+    List<Object?>? whereArgs,
+    ConflictAlgorithm? conflictAlgorithm,
+  }) async {
+    final db = await _instance.db;
+    return await db.update(
+      table,
+      values,
+      where: where,
+      whereArgs: whereArgs,
+      conflictAlgorithm: conflictAlgorithm,
+    );
+  }
+
+  Future<int> delete(
+    String table, {
+    String? where,
+    List<Object?>? whereArgs,
+  }) async {
+    final db = await _instance.db;
+    return await db.delete(
+      table,
+      where: where,
+      whereArgs: whereArgs,
     );
   }
 }
